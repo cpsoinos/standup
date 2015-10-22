13db23e54fb02d02202654b43f22fb1830f01ba4
Corey Psoinos <coreypsoinos@gmail.com>
2015-10-21 22:02:27 -0400

    add contact id to form from ddslick select menu

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,7 +1,15 @@
 class UpdatesController < ApplicationController
 
   def create
+    binding.pry
     @update = Update.new(update_params)
+    if @update.save
+      flash[:notice] = "Sent!"
+      redirect_to home_path
+    else
+      flash[:alert] = "ERROR!"
+      redirect_to :back
+    end
   end
 
   protected
